67ee17c406e081ef35c9d8e8f7032a55f70b714b
YarkovenkoDev <99531855+YarkovenkoDev@users.noreply.github.com>
2022-07-17 00:30:06 +0500

    Add project

---
+++ Magic_The_Gathering_API/Magic_The_Gathering_API/Sources/Scenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic_The_Gathering_API
+//
+//  Created by Daniil Yarkovenko on 17.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
